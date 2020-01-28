7bd9d7eec6f52affd96ae441275a33c605168230
Michael Waldo <mbwcogic@yahoo.com>
2020-01-28 05:18:25 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song 
+  attr_accessor :name 
+  attr_reader :genre, :artist 
+  
+  @@all = []
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+    @@all << self 
+  end 
+  def self.all
+    @@all
+  end 
+  # def say_hi
+   # puts 'hi'
+ # end
+end 
+    
