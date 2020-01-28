4e8e1c0d15c57c3f91676796bbfeee3349bd3a14
Michael Waldo <mbwcogic@yahoo.com>
2020-01-28 05:18:48 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,18 @@
-class 
+class Doctor 
+attr_accessor :name
+
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end 
+  def self.all 
+    @@all 
+  end 
+  #def appointments
+    #Appointment.all.select {|appointment| appointment.doctor == self}
+  #end
+  
+end
+ 
