653eaad2d8b5569ddabfc6aa145f494b8ba0b5e4
Michael Waldo <mbwcogic@yahoo.com>
2020-01-28 05:21:53 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -10,8 +10,8 @@ attr_accessor :name
   def self.all 
     @@all 
   end 
-  #def appointments
-    #Appointment.all.select {|appointment| appointment.doctor == self}
-  #end
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
   
 end
